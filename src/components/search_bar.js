ce3d8be88ef9e5e4bf23b51688caaf9bf078df4b
Bestin <bestinjohn1993@gmail.com>
2017-07-22 18:47:52 +0530

    Convert Input into a controlled component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,15 +4,19 @@ class SearchBar extends Component {
 	constructor(props) {
 		// calling parent(ie Component) methods constructor
 		super(props);
-
+		
+		// This assignment is done only once that too only inside the constructor
 		this.state = { term: '' };
 	}
 	render() {
 		// only manipulate state using setState
+		// Controlled components-->when the value is determined by state
 		return (
 			<div>
-				<input onChange={(event) => { this.setState({ term: event.target.value }); }} />
-				The value of the input is : {this.state.term}
+				<input 
+					value={this.state.term}
+					onChange={(event) => { this.setState({ term: event.target.value }); }} 
+				/>
 			</div>
 			
 
