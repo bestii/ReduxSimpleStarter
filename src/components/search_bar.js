0367cdee81f494a169ff942e603a0ae810c96b2c
Bestin <bestinjohn1993@gmail.com>
2017-07-22 10:34:55 +0530

    Added state to SearchBar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
-    render(){
+	constructor(props){
+		// calling parent(ie Component) methods constructor
+		super(props);
+
+		this.state = { term : ''};
+	}
+	render(){
         return (<input onChange={(event) => { console.log(event.target.value)}} />);
 	};
 
