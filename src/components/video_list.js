a1b4e6f9fdefe83008419e329d7c3f9b5bdde018
Bestin <bestinjohn1993@gmail.com>
2017-07-22 23:56:23 +0530

    Created video_list_item and used it to create the list

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
+    const videoItems = props.videos.map((video) => {
+        // always specify a key for array of items inorder to uniquely identify each item
+        return (<VideoListItem key={video.etag} video={video} />);
+    });
     return (
         // In React we give className instead of class
         <ul className="col-md-4 list-group">
-            {props.videos.length}
+            {videoItems}
         </ul>
     );
 };
