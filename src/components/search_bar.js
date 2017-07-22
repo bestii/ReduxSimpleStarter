d704bdb271670b0a895bcacfeed2f7711626aaec
Bestin <bestinjohn1993@gmail.com>
2017-07-22 10:22:12 +0530

    Event handlers in React

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,8 +2,12 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
     render(){
-        return <input/>;
-    }
+        return (<input onChange={this.onInputChange} />);
+	}
+	
+	onInputChange(event){
+		console.log(event.target.value);
+	}
 }
 
 export default SearchBar;
